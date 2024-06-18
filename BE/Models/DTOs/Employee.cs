87139f1e0bf8c9242292a87d5171520eb50b06f2
phuongndHE153051 <105622118+phuongndHE153051@users.noreply.github.com>
2024-06-18 12:52:48 +0700

    dc

---
+++ BE/Models/DTOs/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace BE.Models.DTOs
+{
+    public class Employee
+    {
+        public string Name { get; set; } = null!;
+
+        public string Gender { get; set; } = null!;
+
+        public int Age { get; set; }
+
+        public string Phone { get; set; } = null!;
+
+        public string Role { get; set; }
+    }
+}
