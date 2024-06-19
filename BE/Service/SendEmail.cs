0ffd772dfee099e172237e4e74d3c221f19d6bc1
ducanh0310 <nguyenducanhflm@gmail.com>
2024-06-19 22:45:02 +0700

    Update SendEmail.cs

--- BE/Service/SendEmail.cs
+++ BE/Service/SendEmail.cs
@@ -15,7 +15,7 @@ namespace BE.Service
         public async Task SendEmailAsync(string toEmail, string subject, string body)
         {
             var client = new SendGridClient(_sendGridApiKey);
-            var from = new EmailAddress("your-email@example.com", "Your Name");
+            var from = new EmailAddress("anhndhe160131@fpt.edu.vn", "Your Name");
             var to = new EmailAddress(toEmail);
             var msg = MailHelper.CreateSingleEmail(from, to, subject, body, body);
             var response = await client.SendEmailAsync(msg);
