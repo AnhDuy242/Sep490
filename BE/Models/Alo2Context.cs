39e3015bbb997bff6eae962b7c2d123b0e5aaafa
phuongndHE153051 <105622118+phuongndHE153051@users.noreply.github.com>
2024-06-20 15:32:20 +0700

    dbcontext_Change

--- BE/Models/Alo2Context.cs
+++ BE/Models/Alo2Context.cs
@@ -60,10 +60,15 @@ public partial class Alo2Context : DbContext
     public virtual DbSet<Week> Weeks { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("server=DUCANH\\SQLEXPRESS; database=alo2;uid=sa;pwd=123456;Trusted_Connection=True;Encrypt=False");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        }
+    }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Account>(entity =>
         {
