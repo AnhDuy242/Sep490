840b94a241f8256002675875bf5872f2118f6067
trinhthanhmyhihihaha <113117754+mytthe153136@users.noreply.github.com>
2024-06-27 12:37:20 +0700

    protected route
    hiện tại mới chỉ add protected mà chưa add cụ thể phân role nào sẽ vào được những chỗ nào

--- FE/src/App.js
+++ FE/src/App.js
@@ -4,15 +4,15 @@ import Home from './pages/Home';
 import AdminDash from '../src/pages/AdminDashBoard/index'; // Sửa lại đường dẫn nếu cần
 import DoctorAccount from './pages/AdminDashBoard/doctor_account'; // Điều chỉnh đường dẫn tùy vào cấu trúc project của bạn
 import ReceptionistAccount from './pages/AdminDashBoard/receptionist_account'; // Điều chỉnh đường dẫn tùy vào cấu trúc project của bạn
-
+import ProtectedRoute from './pages/AdminDashBoard/component/protected_route';
 function App() {
   return (
 
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/admin/dashboard" element={<AdminDash/>}>
-        <Route path="doctor-account" element={<DoctorAccount/>} />
-        <Route path="receptionist-account" element={<ReceptionistAccount/>} />
+      <Route path="/admin/dashboard" element={<ProtectedRoute requiredRole="Admin"><AdminDash /></ProtectedRoute>}>
+      <Route path="/admin/dashboard/doctor-account" element={<DoctorAccount/>} />
+        <Route path="/admin/dashboard/receptionist-account" element={<ReceptionistAccount/>} />
       </Route>
     </Routes>
 
