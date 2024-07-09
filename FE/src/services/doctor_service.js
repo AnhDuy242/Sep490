997e991186834797e8755fdfa645c50d0cbf3c62
Anthuyeen <87635968+Anthuyeen@users.noreply.github.com>
2024-07-09 14:12:26 +0700

    Update doctor return json file

--- FE/src/services/doctor_service.js
+++ FE/src/services/doctor_service.js
@@ -35,63 +35,6 @@ export const addDoctor = async (doctor) => {
 };
 
 // Hàm để chỉnh sửa thông tin bác sĩ
-// export const updateDoctor = async (id, data) => {
-//   const url = `https://localhost:7240/api/Doctor/${id}`;
-
-//   try {
-//     const response = await fetch(url, {
-//       method: 'PUT', // Phương thức PUT để cập nhật dữ liệu
-//       headers: {
-//         'Content-Type': 'application/json' // Đặt loại nội dung là JSON
-//       },
-//       body: JSON.stringify(data) // Chuyển đổi đối tượng JavaScript thành chuỗi JSON
-//     });
-
-//     if (!response.ok) {
-//       const error = await response.json();
-//       throw new Error(`Error: ${error.message}`);
-//     }
-
-//     const updatedDoctor = await response.json();
-//     return updatedDoctor;
-//   } catch (error) {
-//     console.error('Error updating doctor:', error);
-//     throw error;
-//   }
-// };
-
-// export const updateDoctor = async (id, data) => {
-//   const url = `https://localhost:7240/api/Doctor/${id}`;
-
-//   console.log('Update Doctor URL:', url);
-//   console.log('Update Doctor Data:', data);
-
-//   try {
-//     const response = await fetch(url, {
-//       method: 'PUT',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify(data)
-//     });
-
-//     console.log('Response Status:', response.status);
-
-//     if (!response.ok) {
-//       const errorText = await response.text();
-//       console.log('API Error Response:', errorText);
-//       throw new Error(`Error: ${response.status} ${response.statusText}`);
-//     }
-
-//     const updatedDoctor = await response.json();
-//     return updatedDoctor;
-//   } catch (error) {
-//     console.error('Error updating doctor:', error);
-//     throw error;
-//   }
-// };
-
-
 export const updateDoctor = async (id, data) => {
   const url = `https://localhost:7240/api/Doctor/${id}`;
 
