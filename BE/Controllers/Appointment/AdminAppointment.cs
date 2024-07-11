66c74649a66584c2361b4350dec91f7a348338b6
ducanh0310 <nguyenducanhflm@gmail.com>
2024-07-11 11:48:31 +0700

    Create AdminAppointment.cs

---
+++ BE/Controllers/Appointment/AdminAppointment.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BE.Controllers.Appointment
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AdminAppointment : ControllerBase
+    {
+    }
+}
