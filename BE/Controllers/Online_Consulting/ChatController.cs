86409533349ee8d33101eb9d26e9bf33e3106758
phuongndHE153051 <105622118+phuongndHE153051@users.noreply.github.com>
2024-07-18 10:20:37 +0700

    Create File

---
+++ BE/Controllers/Online_Consulting/ChatController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BE.Controllers.Online_Consulting
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController : ControllerBase
+    {
+    }
+}
