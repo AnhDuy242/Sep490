40fe8b2baf5644f88b3ce836ffce27e27c4405b9
phuongndHE153051 <105622118+phuongndHE153051@users.noreply.github.com>
2024-07-18 11:54:31 +0700

    ChatOnlineApi

--- BE/Controllers/Online_Consulting/ChatController.cs
+++ BE/Controllers/Online_Consulting/ChatController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BE.DTOs;
+using BE.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 
 namespace BE.Controllers.Online_Consulting
 {
@@ -7,5 +10,24 @@ namespace BE.Controllers.Online_Consulting
     [ApiController]
     public class ChatController : ControllerBase
     {
+        private readonly IHubContext<ChatHub> _hubContext;
+        public ChatController(IHubContext<ChatHub> hubContext)
+        {
+            _hubContext = hubContext;
+        }
+
+
+        [HttpPost("send")]
+        public async Task<IActionResult> SendMessage([FromBody] ChatMessage message)
+        {
+            if (message == null || string.IsNullOrEmpty(message.User) || string.IsNullOrEmpty(message.Text))
+            {
+                return BadRequest("Invalid message.");
+            }
+
+            await _hubContext.Clients.All.SendAsync("ReceiveMessage", message.User, message.Text);
+
+            return Ok(new { Status = "Message sent successfully." });
+        }
     }
 }
---
+++ BE/DTOs/ChatMessage.cs
@@ -0,0 +1,8 @@
+﻿namespace BE.DTOs
+{
+    public class ChatMessage
+    {
+        public string User { get; set; }
+        public string Text { get; set; }
+    }
+}
