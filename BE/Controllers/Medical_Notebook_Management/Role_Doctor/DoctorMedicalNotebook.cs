0b220e448f0f5d625162edbd9c308eac63c11a84
ducanh0310 <nguyenducanhflm@gmail.com>
2024-07-22 11:00:57 +0700

    Update DoctorMedicalNotebook.cs

--- BE/Controllers/Medical_Notebook_Management/Role_Doctor/DoctorMedicalNotebook.cs
+++ BE/Controllers/Medical_Notebook_Management/Role_Doctor/DoctorMedicalNotebook.cs
@@ -3,6 +3,8 @@ using BE.DTOs.MedicalNoteBookDro;
 using BE.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 
 namespace BE.Controllers.Medical_Notebook_Management.Role_Doctor
 {
@@ -38,7 +40,14 @@ namespace BE.Controllers.Medical_Notebook_Management.Role_Doctor
             return Ok(medicalNoteBook);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> ViewMedicalNoteBookByPatientId(int pid)
+        {
+            var list = _context.MedicalNotebooks.Include(x => x.Patient).Include(x => x.Doctor).Where(x => x.PatientId == pid).ToList();
+            var lists = _mapper.Map<List<MedicalNotebookPatient>>(list);
+            return Ok(lists);
 
+        }
 
     }
 }
