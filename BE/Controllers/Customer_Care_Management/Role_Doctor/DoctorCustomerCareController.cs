625f7cffaa9b580cb69ed7f58a9084e3011cdd06
ducanh0310 <nguyenducanhflm@gmail.com>
2024-07-27 20:24:28 +0700

    Update DoctorCustomerCareController.cs

--- BE/Controllers/Customer_Care_Management/Role_Doctor/DoctorCustomerCareController.cs
+++ BE/Controllers/Customer_Care_Management/Role_Doctor/DoctorCustomerCareController.cs
@@ -1,11 +1,14 @@
 ﻿using AutoMapper;
+using BE.DTOs.DoctorDto;
+using BE.DTOs.PatientDto;
 using BE.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BE.Controllers.Customer_Care_Management.Role_Doctor
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class DoctorCustomerCareController : ControllerBase
     {
@@ -15,6 +18,40 @@ namespace BE.Controllers.Customer_Care_Management.Role_Doctor
         {
             _context = context;
             _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetListPatientByDoctorId(int docid)
+        {
+            var patients = await _context.MedicalNotebooks
+             .Where(mn => mn.DoctorId == docid)
+             .Select(mn => mn.Patient)
+             .Distinct()
+             .ToListAsync();
+
+            if (patients == null || !patients.Any())
+            {
+                return NotFound();
+            }
+            var p = _mapper.Map<List<PatientReceptionist>>(patients);
+            return Ok(p);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetListDoctorByPatientId(int pid)
+        {
+            var doctors = await _context.MedicalNotebooks
+                .Where(mn => mn.PatientId == pid)
+                .Select(mn => mn.Doctor)
+                .Distinct()
+                .ToListAsync();
+
+            if (doctors == null || !doctors.Any())
+            {
+                return NotFound();
+            }
+            var p = _mapper.Map<List<DoctorMarketing>>(doctors);
+
+            return Ok(p);
         }
     }
 }
